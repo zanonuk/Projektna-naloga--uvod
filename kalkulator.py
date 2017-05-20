39128a6c160d056d886a97ad1d8947cf14740554
zanonuk <zan.onuk@gmail.com>
2017-05-20 15:07:49 +0200

    posodobljeno 1

--- kalkulator.py
+++ kalkulator.py
@@ -3,7 +3,7 @@ okno = tk.Tk()
 vnosno_polje = tk.Entry(okno)
 gumbi = tk.Frame(okno)
 
-gumb = tk.Button(okno, text = "Nova igra 55")
+gumb = tk.Button(okno, text = "Nova igra")
 gumb.place(x=50, y=50)
 
 
